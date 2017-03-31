4717d8053de0f506badce5174a600a3eabb2c8d3
i.ivanina <ilya.ivanina@gmail.com>
2017-03-31 19:44:44 +0300

    init

---
+++ src/main/java/org/ivanina/dev/CurrentThreadDemo.java
@@ -0,0 +1,19 @@
+package org.ivanina.dev;
+
+public class CurrentThreadDemo {
+    public static void main(String arg[]){
+        Thread currentThread = Thread.currentThread();
+        System.out.println("Current thread: "+currentThread.getName());
+        currentThread.setName("CurrentThreadDemo");
+        System.out.println("Current thread renamed: "+currentThread);
+        try {
+            for(int i=0; i<5; i++){
+                System.out.println(i);
+                Thread.sleep(1000);
+            }
+        }catch (InterruptedException e){
+            System.out.println(e.getMessage());
+        }
+
+    }
+}
